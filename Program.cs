4a1d46e8b833f94d9b6d2d34a015e2459adeda51
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-10 04:24:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter an number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter another number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+
+
+            }
+            catch (Exception e)
+            {  //can aslo have one generic error message, or catch the exact error.
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
